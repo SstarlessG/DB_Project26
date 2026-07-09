2886880ca09789f0f2a452dfc37da061a8185100
Kjanray <kzhanrui@gmail.com>
2026-07-09 21:31:10 +0800

    feat(Ex 4): Implemented features needed for delete and insert operations

--- src/java/simpledb/execution/Delete.java
+++ src/java/simpledb/execution/Delete.java
@@ -20,6 +20,11 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private TransactionId t;
+    private OpIterator child;
+    private TupleDesc td;
+    private boolean alreadyDeleted;
+
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -31,23 +36,33 @@ public class Delete extends Operator {
      */
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        this.t = t;
+        this.child = child;
+        this.td = new TupleDesc(new Type[]{Type.INT_TYPE});
+        this.alreadyDeleted = false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        child.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
+        alreadyDeleted = false;
     }
 
     /**
@@ -61,18 +76,44 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+
+        // If already deleted, return null
+        if (alreadyDeleted) {
+            return null;
+        } 
+
+        // Count the number of deleted tuples
+        int count = 0;
+        while (child.hasNext()) {
+            Tuple tuple = child.next();
+            try {
+                Database.getBufferPool().deleteTuple(t, tuple);
+                count++;
+            } catch (IOException e) {
+                throw new DbException("Error deleting tuple: " + e.getMessage());
+            }
+        }
+
+        // Create a new tuple to return the count of deleted records
+        Tuple result = new Tuple(td);
+        result.setField(0, new IntField(count));
+        alreadyDeleted = true;
+        return result;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (children.length != 1) {
+            throw new IllegalArgumentException("Expected exactly one child");
+        }
+        this.child = children[0];
     }
 
 }
--- src/java/simpledb/execution/Insert.java
+++ src/java/simpledb/execution/Insert.java
@@ -1,8 +1,12 @@
 package simpledb.execution;
 
+import java.io.IOException;
+
 import simpledb.common.Database;
 import simpledb.common.DbException;
+import simpledb.common.Type;
 import simpledb.storage.BufferPool;
+import simpledb.storage.IntField;
 import simpledb.storage.Tuple;
 import simpledb.storage.TupleDesc;
 import simpledb.transaction.TransactionAbortedException;
@@ -15,6 +19,12 @@ import simpledb.transaction.TransactionId;
 public class Insert extends Operator {
 
     private static final long serialVersionUID = 1L;
+
+    private TransactionId t;
+    private OpIterator child;
+    private int tableId;
+    private TupleDesc td;
+    private boolean alreadyInserted;
 
     /**
      * Constructor.
@@ -32,23 +42,41 @@ public class Insert extends Operator {
     public Insert(TransactionId t, OpIterator child, int tableId)
             throws DbException {
         // some code goes here
+        this.t = t;
+        this.child = child;
+        this.tableId = tableId;
+
+        TupleDesc childTd = child.getTupleDesc();
+        TupleDesc expectedTd = Database.getCatalog().getTupleDesc(tableId);
+        if (!childTd.equals(expectedTd)) {
+            throw new DbException("TupleDesc mismatch");
+        }
+        this.td = new TupleDesc(new Type[]{Type.INT_TYPE});
+        this.alreadyInserted = false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        child.open();
+
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
+        alreadyInserted = false;
     }
 
     /**
@@ -66,17 +94,43 @@ public class Insert extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+
+        // If already inserted, return null
+        if (alreadyInserted) {
+            return null;
+        }
+        alreadyInserted = true;
+
+        // insert all tuples from child into the table
+        int count = 0;
+        while (child.hasNext()) {
+            Tuple tuple = child.next();
+            try {
+                Database.getBufferPool().insertTuple(t, tableId, tuple);
+                count++;
+            } catch (IOException e) {
+                throw new DbException("Error inserting tuple: " + e.getMessage());
+            }
+        }
+
+        // create a new tuple to return the count of inserted records
+        Tuple resultTuple = new Tuple(getTupleDesc());
+        resultTuple.setField(0, new IntField(count));
+        return resultTuple;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[] { child };
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (children.length != 1) {
+            throw new IllegalArgumentException("Expected exactly one child");
+        }
+        this.child = children[0];
     }
 }
